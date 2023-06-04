9ef49bc0aeab022d8c635fd7715c45d45c3c6259
NadyaGolub <nadyagolub@ukr.net>
2023-06-04 20:26:57 +0300

    completing homework

--- models/contact.js
+++ models/contact.js
@@ -30,30 +30,6 @@ const contactSchema = new Schema({
 
 contactSchema.post("save", handleMongooseError);
 
-// const contactAddSchema = Joi.object({
-//   name: Joi.string()
-//     .required()
-//     .messages({ "any.required": `missing required name field` }),
-//   email: Joi.string()
-//     .required()
-//     .messages({ "any.required": `missing required name field` }),
-//   phone: Joi.string()
-//     .required()
-//         .messages({ "any.required": `missing required name field` }),
-//   favorite: Joi.boolean(),
-// });
-
-// const favoriteSchema = Joi.object({
-//   favorite: Joi.boolean()
-//     .required()
-//     .messages({ "any.required": "missing field favorite" }),
-// });
-
-// const schemas = {
-//     contactAddSchema,
-//     favoriteSchema,
-// }
-
 const Contact = model("contact", contactSchema);
 
 module.exports = {
